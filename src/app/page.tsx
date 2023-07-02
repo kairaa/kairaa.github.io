9344fa210864b46dd3d35923308c0188de41f28d
Kayra Çakıroğlu <1030510099@erciyes.edu.tr>
2023-07-02 15:07:16 +0300

    bir deneme daha

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>melabaaa</h1>
+      <h1 className="text-6xl font-normal leading-normal mt-0 mb-2 text-pink-800">
+        melabaaa
+      </h1>
       <p>burada bazı seyler olacaaagk</p>
     </main>
   );
