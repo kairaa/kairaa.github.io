be3d752048cde631f0f10074e7deb73f6bad0713
Kayra Çakıroğlu <1030510099@erciyes.edu.tr>
2023-07-02 17:12:11 +0300

    basePath removed for testing purpose

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "/kairaa.github.io",
+  // basePath: "/kairaa.github.io",
 
   /**
    * Disable server-based image optimization. Next.js does not support
